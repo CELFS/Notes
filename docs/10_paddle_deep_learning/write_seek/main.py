092134ec8b62d21e2e694ce3a369d2eeb495951c
CELFS <634798049@qq.com>
2023-03-27 04:48:07 +0800

    paddle dl 第三周笔记

--- docs/10_paddle_deep_learning/write_seek/main.py
+++
@@ -1,5 +0,0 @@
-with open(r"task03.md", 'r+', encoding="UTF8") as fw:
-    with open(r'copy.md', 'x+', encoding="UTF8") as w:
-        for line in fw.readlines():
-            res = line.replace('D:\\TyporaTxt\\PicCopy\\', "test/")
-            w.write(f"{res}")
